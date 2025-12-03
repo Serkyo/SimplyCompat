3c8effd018791c873d2b291abbf08af404322224
Serkyo <serkyo-dev@proton.me>
2025-12-03 09:35:20 +0100

    Added a helper class for custom tiers

---
+++ src/main/java/com/serkyo/simplycompat/utils/CustomTiers.java
@@ -0,0 +1,59 @@
+package com.serkyo.simplycompat.utils;
+
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public enum CustomTiers implements Tier {
+    COPPER(2, 300, 0.0F, 0.7F, 10, () -> Ingredient.of(Items.COPPER_INGOT));
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    CustomTiers(int pLevel, int pUses, float pSpeed, float pDamage, int pEnchantmentValue, Supplier<Ingredient> pRepairIngredient) {
+        this.level = pLevel;
+        this.uses = pUses;
+        this.speed = pSpeed;
+        this.damage = pDamage;
+        this.enchantmentValue = pEnchantmentValue;
+        this.repairIngredient = new LazyLoadedValue<>(pRepairIngredient);
+    }
+
+    @Override
+    public int getUses() {
+        return uses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return damage;
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public @NotNull Ingredient getRepairIngredient() {
+        return repairIngredient.get();
+    }
+}
