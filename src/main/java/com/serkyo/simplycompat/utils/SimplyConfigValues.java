7974acc2fbe1e753d9d548857edf1742532e0068
Serkyo <serkyo-dev@proton.me>
2025-12-03 09:35:38 +0100

    Added a helper class to retrieve config values from Simply mods

---
+++ src/main/java/com/serkyo/simplycompat/utils/SimplyConfigValues.java
@@ -0,0 +1,58 @@
+package com.serkyo.simplycompat.utils;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import net.rosemarythyme.simplymore.config.WeaponAttributesConfig;
+import net.rosemarythyme.simplymore.config.WrapperConfig;
+import net.sweenus.simplyswords.config.Config;
+import net.sweenus.simplyswords.config.ConfigDefaultValues;
+
+public class SimplyConfigValues {
+    public static final float LONGSWORD_POSITIVE_MODIFIER = Config.getFloat("longsword_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.longsword_positiveDamageModifier);
+    public static final float TWINBLADE_POSITIVE_MODIFIER = Config.getFloat("twinblade_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.twinblade_positiveDamageModifier);
+    public static final float RAPIER_POSITIVE_MODIFIER = Config.getFloat("rapier_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.rapier_positiveDamageModifier);
+    public static final float KATANA_POSITIVE_MODIFIER = Config.getFloat("katana_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.katana_positiveDamageModifier);
+    public static final float SAI_POSITIVE_MODIFIER = Config.getFloat("sai_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.sai_positiveDamageModifier);
+    public static final float SPEAR_POSITIVE_MODIFIER = Config.getFloat("spear_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.spear_positiveDamageModifier);
+    public static final float GLAIVE_POSITIVE_MODIFIER = Config.getFloat("glaive_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.glaive_positiveDamageModifier);
+    public static final float WARGLAIVE_POSITIVE_MODIFIER = Config.getFloat("warglaive_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.warglaive_positiveDamageModifier);
+    public static final float CUTLASS_POSITIVE_MODIFIER = Config.getFloat("cutlass_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.cutlass_positiveDamageModifier);
+    public static final float CLAYMORE_POSITIVE_MODIFIER = Config.getFloat("claymore_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.claymore_positiveDamageModifier);
+    public static final float GREATAXE_POSITIVE_MODIFIER = Config.getFloat("greataxe_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.greataxe_positiveDamageModifier);
+    public static final float GREATHAMMER_POSITIVE_MODIFIER = Config.getFloat("greathammer_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.greathammer_positiveDamageModifier);
+    public static final float CHAKRAM_POSITIVE_MODIFIER = Config.getFloat("chakram_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.chakram_positiveDamageModifier);
+    public static final float SCYTHE_POSITIVE_MODIFIER = Config.getFloat("scythe_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.scythe_positiveDamageModifier);
+    public static final float HALBERD_POSITIVE_MODIFIER = Config.getFloat("halberd_positiveDamageModifier", "WeaponAttributes", ConfigDefaultValues.halberd_positiveDamageModifier);
+    public static final float LONGSWORD_NEGATIVE_MODIFIER = Config.getFloat("longsword_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.longsword_negativeDamageModifier);
+    public static final float TWINBLADE_NEGATIVE_MODIFIER = Config.getFloat("twinblade_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.twinblade_negativeDamageModifier);
+    public static final float RAPIER_NEGATIVE_MODIFIER = Config.getFloat("rapier_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.rapier_negativeDamageModifier);
+    public static final float SAI_NEGATIVE_MODIFIER = Config.getFloat("sai_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.sai_negativeDamageModifier);
+    public static final float SPEAR_NEGATIVE_MODIFIER = Config.getFloat("spear_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.spear_negativeDamageModifier);
+    public static final float KATANA_NEGATIVE_MODIFIER = Config.getFloat("katana_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.katana_negativeDamageModifier);
+    public static final float GLAIVE_NEGATIVE_MODIFIER = Config.getFloat("glaive_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.glaive_negativeDamageModifier);
+    public static final float WARGLAIVE_NEGATIVE_MODIFIER = Config.getFloat("warglaive_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.warglaive_negativeDamageModifier);
+    public static final float CUTLASS_NEGATIVE_MODIFIER = Config.getFloat("cutlass_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.cutlass_negativeDamageModifier);
+    public static final float CLAYMORE_NEGATIVE_MODIFIER = Config.getFloat("claymore_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.claymore_negativeDamageModifier);
+    public static final float GREATAXE_NEGATIVE_MODIFIER = Config.getFloat("greataxe_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.greataxe_negativeDamageModifier);
+    public static final float GREATHAMMER_NEGATIVE_MODIFIER = Config.getFloat("greathammer_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.greathammer_negativeDamageModifier);
+    public static final float CHAKRAM_NEGATIVE_MODIFIER = Config.getFloat("chakram_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.chakram_negativeDamageModifier);
+    public static final float SCYTHE_NEGATIVE_MODIFIER = Config.getFloat("scythe_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.scythe_negativeDamageModifier);
+    public static final float HALBERD_NEGATIVE_MODIFIER = Config.getFloat("halberd_negativeDamageModifier", "WeaponAttributes", ConfigDefaultValues.halberd_negativeDamageModifier);
+    public static final float LONGSWORD_ATTACKSPEED = Config.getFloat("longsword_attackSpeed", "WeaponAttributes", ConfigDefaultValues.longsword_attackSpeed);
+    public static final float TWINBLADE_ATTACKSPEED = Config.getFloat("twinblade_attackSpeed", "WeaponAttributes", ConfigDefaultValues.twinblade_attackSpeed);
+    public static final float RAPIER_ATTACKSPEED = Config.getFloat("rapier_attackSpeed", "WeaponAttributes", ConfigDefaultValues.rapier_attackSpeed);
+    public static final float SAI_ATTACKSPEED = Config.getFloat("sai_attackSpeed", "WeaponAttributes", ConfigDefaultValues.sai_attackSpeed);
+    public static final float SPEAR_ATTACKSPEED = Config.getFloat("spear_attackSpeed", "WeaponAttributes", ConfigDefaultValues.spear_attackSpeed);
+    public static final float KATANA_ATTACKSPEED = Config.getFloat("katana_attackSpeed", "WeaponAttributes", ConfigDefaultValues.katana_attackSpeed);
+    public static final float GLAIVE_ATTACKSPEED = Config.getFloat("glaive_attackSpeed", "WeaponAttributes", ConfigDefaultValues.glaive_attackSpeed);
+    public static final float WARGLAIVE_ATTACKSPEED = Config.getFloat("warglaive_attackSpeed", "WeaponAttributes", ConfigDefaultValues.warglaive_attackSpeed);
+    public static final float CUTLASS_ATTACKSPEED = Config.getFloat("cutlass_attackSpeed", "WeaponAttributes", ConfigDefaultValues.cutlass_attackSpeed);
+    public static final float CLAYMORE_ATTACKSPEED = Config.getFloat("claymore_attackSpeed", "WeaponAttributes", ConfigDefaultValues.claymore_attackSpeed);
+    public static final float GREATAXE_ATTACKSPEED = Config.getFloat("greataxe_attackSpeed", "WeaponAttributes", ConfigDefaultValues.greataxe_attackSpeed);
+    public static final float GREATHAMMER_ATTACKSPEED = Config.getFloat("greathammer_attackSpeed", "WeaponAttributes", ConfigDefaultValues.greathammer_attackSpeed);
+    public static final float CHAKRAM_ATTACKSPEED = Config.getFloat("chakram_attackSpeed", "WeaponAttributes", ConfigDefaultValues.chakram_attackSpeed);
+    public static final float SCYTHE_ATTACKSPEED = Config.getFloat("scythe_attackSpeed", "WeaponAttributes", ConfigDefaultValues.scythe_attackSpeed);
+    public static final float HALBERD_ATTACKSPEED = Config.getFloat("halberd_attackSpeed", "WeaponAttributes", ConfigDefaultValues.halberd_attackSpeed);
+
+    private static final WrapperConfig SIMPLY_MORE_CONFIG = AutoConfig.getConfigHolder(WrapperConfig.class).getConfig();
+    public static WeaponAttributesConfig attributesSimplyMore = SIMPLY_MORE_CONFIG.weaponAttributes;
+}
