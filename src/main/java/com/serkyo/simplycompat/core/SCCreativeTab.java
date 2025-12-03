b35d0f006f53b8002b36f354a566e81ed64862ee
Serkyo <serkyo-dev@proton.me>
2025-12-03 10:14:13 +0100

    Added IAF compat with copper tools

--- src/main/java/com/serkyo/simplycompat/core/SCCreativeTab.java
+++ src/main/java/com/serkyo/simplycompat/core/SCCreativeTab.java
@@ -1,6 +1,7 @@
 package com.serkyo.simplycompat.core;
 
 import com.serkyo.simplycompat.SimplyCompat;
+import com.serkyo.simplycompat.compat.IceAndFireCompat;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -18,7 +19,31 @@ public class SCCreativeTab {
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.fromNamespaceAndPath("simplyswords", "empowered_remnant"))))
             .title(Component.translatable("creativetab.simplycompat_tab"))
             .displayItems((pParameters, pOutput) -> {
-
+                pOutput.accept(IceAndFireCompat.COPPER_LONGSWORD.get());
+                pOutput.accept(IceAndFireCompat.COPPER_TWINBLADE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_RAPIER.get());
+                pOutput.accept(IceAndFireCompat.COPPER_KATANA.get());
+                pOutput.accept(IceAndFireCompat.COPPER_SAI.get());
+                pOutput.accept(IceAndFireCompat.COPPER_SPEAR.get());
+                pOutput.accept(IceAndFireCompat.COPPER_GLAIVE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_WARGLAIVE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_CUTLASS.get());
+                pOutput.accept(IceAndFireCompat.COPPER_CLAYMORE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_GREATHAMMER.get());
+                pOutput.accept(IceAndFireCompat.COPPER_GREATAXE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_CHAKRAM.get());
+                pOutput.accept(IceAndFireCompat.COPPER_SCYTHE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_HALBERD.get());
+                pOutput.accept(IceAndFireCompat.COPPER_GREAT_KATANA.get());
+                pOutput.accept(IceAndFireCompat.COPPER_GRANDSWORD.get());
+                pOutput.accept(IceAndFireCompat.COPPER_BACKHAND_BLADE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_LANCE.get());
+                pOutput.accept(IceAndFireCompat.COPPER_KHOPESH.get());
+                pOutput.accept(IceAndFireCompat.COPPER_DAGGER.get());
+                pOutput.accept(IceAndFireCompat.COPPER_PERNACH.get());
+                pOutput.accept(IceAndFireCompat.COPPER_QUARTERSTAFF.get());
+                pOutput.accept(IceAndFireCompat.COPPER_GREAT_SPEAR.get());
+                pOutput.accept(IceAndFireCompat.COPPER_DEER_HORNS.get());
             })
             .build());
 
--- src/main/java/com/serkyo/simplycompat/core/SCItems.java
+++ src/main/java/com/serkyo/simplycompat/core/SCItems.java
@@ -1,15 +1,12 @@
 package com.serkyo.simplycompat.core;
 
-import com.serkyo.simplycompat.SimplyCompat;
-import net.minecraft.world.item.Item;
+import com.serkyo.simplycompat.compat.IceAndFireCompat;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class SCItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SimplyCompat.MOD_ID);
-
     public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
+        if (IceAndFireCompat.isPresent()) {
+            IceAndFireCompat.IAF_COMPAT_ITEMS.register(eventBus);
+        }
     }
 }
