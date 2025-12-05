e7e7b7862ac8c359df0c18ddcf113127fb38fbc5
Serkyo <serkyo-dev@proton.me>
2025-12-05 22:38:29 +0100

    Fixed SimplyCompat items appearing in SimplySwords creative menu

--- src/main/java/com/serkyo/simplycompat/core/SCCreativeTab.java
+++ src/main/java/com/serkyo/simplycompat/core/SCCreativeTab.java
@@ -2,16 +2,26 @@ package com.serkyo.simplycompat.core;
 
 import com.serkyo.simplycompat.SimplyCompat;
 import com.serkyo.simplycompat.compat.IceAndFireCompat;
+import com.serkyo.simplycompat.item.SCSimplySword;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.util.MutableHashedLinkedMap;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.Iterator;
+import java.util.Map;
+
+@Mod.EventBusSubscriber(modid = SimplyCompat.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class SCCreativeTab {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, SimplyCompat.MOD_ID);
 
@@ -20,12 +30,30 @@ public class SCCreativeTab {
             .title(Component.translatable("creativetab.simplycompat_tab"))
             .displayItems((pParameters, pOutput) -> {
                 if (IceAndFireCompat.INSTANCE.isPresent()) {
-                    IceAndFireCompat.INSTANCE.addToCreativeMenu(pOutput);
+                     IceAndFireCompat.INSTANCE.addToCreativeMenu(pOutput);
                 }
             })
             .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+    @SubscribeEvent
+    public static void removeSCItemsFromSS(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey().location().toString().equals("simplyswords:simplyswords")) {
+            MutableHashedLinkedMap<ItemStack, CreativeModeTab.TabVisibility> entries = event.getEntries();
+            Iterator<Map.Entry<ItemStack, CreativeModeTab.TabVisibility>> iterator = entries.iterator();
+
+            while (iterator.hasNext()) {
+                Map.Entry<ItemStack, CreativeModeTab.TabVisibility> entry = iterator.next();
+                ItemStack itemStack = entry.getKey();
+                System.out.println(itemStack.getDisplayName());
+                if (itemStack.getItem() instanceof SCSimplySword) {
+                    iterator.remove();
+                }
+            }
+            SimplyCompat.LOGGER.debug("Cleaned the items from the creative tab of Simply Swords");
+        }
     }
 }
